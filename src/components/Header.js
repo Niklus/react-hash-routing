d4c6a814edd4b16ed5d34caa82af0e274da5d637
Nicholas Otieno <niklasoti@gmail.com>
2020-10-03 00:14:59 +0200

    Update

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => (
+  <header>
+    <a href="#home">Home</a> | <a href="#about">About</a> |{" "}
+    <a href="#contact"> Contact</a>
+    <hr />
+  </header>
+);
+
+export default Header;
